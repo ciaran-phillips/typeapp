bd1ac061c8060d183d7e5f16e56252c39d551271
Ciaran Phillips <ciaranphillips1@gmail.com>
2014-03-22 21:56:48 +0000

    Added angular animate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('typeApp', [
   'ngRoute',
+  'ngAnimate',
   'typeApp.filters',
   'typeApp.services',
   'typeApp.directives',
