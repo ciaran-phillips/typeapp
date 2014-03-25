0d98cf62e0c47b2ca5602edf7f8684be50862ab2
Ciaran Phillips <ciaranphillips1@gmail.com>
2014-03-25 23:35:23 +0000

    Set directives correctly (they were overwriting each other)

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -3,15 +3,14 @@
 /* Directives */
 
 
-angular.module('typeApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+var myApp = angular.module('typeApp.directives', []);
+myApp.directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
   
-  angular.module('typeApp.directives', []).
-  directive('checkboxSwitch', [function() {
+myApp.directive('checkboxSwitch', [function() {
     return {
     	"link":function(scope, elm, attrs) {
 	      
@@ -20,8 +19,7 @@ angular.module('typeApp.directives', []).
     };
   }]);
   
-angular.module('typeApp.directives', []).
-	directive('rowTransform', [function() {
+myApp.directive('rowTransform', [function() {
 		return {
 			"link":function(scope, elm, attrs) {
 				attrs.$observe("size",function(size) {
